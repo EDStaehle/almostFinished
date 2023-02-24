9b2e7efb71ce95704b74a7d2f95c1074f610e48a
Elias <elias.staehle@gmail.com>
2023-02-24 13:39:59 -0500

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+const Header = () => {
+  return (
+    <>
+      <div className='headerCont'>
+        <p>Header</p>
+      </div>
+    </>
+  );
+};
+export default Header;
